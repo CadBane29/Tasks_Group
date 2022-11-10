cb05ab440a022e408540c60b54130380b048b483
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-10 13:31:22 +0300

    add Task1 Colored Flag

---
+++ task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+int ax = 1;
+int ay = 1;
+int bx = 1;
+int by = 2;
+int cx = 1;
+int cy = 3;
+int count = 0;
+while (count<10)
+{
+     Console.SetCursorPosition(ax+count, ay);
+     Console.ForegroundColor = ConsoleColor.White;
+     Console.WriteLine("+");
+     Console.SetCursorPosition(bx+count, by);
+     Console.ForegroundColor = 
+ConsoleColor.Blue
+;
+     Console.WriteLine("+");
+     Console.SetCursorPosition(cx+count, cy);
+     Console.ForegroundColor = 
+ConsoleColor.Red
+;
+     Console.WriteLine("+");
+     count++;
+} 
