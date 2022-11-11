865d5ff9facc903ae40735b52e1fa63f1500efcc
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-11 15:33:37 +0300

    add Task4 Square and Two Number

---
+++ Task4_SquareTwoNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿int firstNumber = new Random().Next(1, 99);
+int secondNumber = new Random().Next(1, 99);
+Console.WriteLine(firstNumber);
+Console.WriteLine(secondNumber);
+if (firstNumber == secondNumber * secondNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+
+else
+{
+    Console.WriteLine("Второе число не является квадратом первого.");
+}
