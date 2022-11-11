8d4878a0ba7186f58c03f2a26c33f7928a3cc1f9
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-11 13:53:00 +0300

    add Unfinished Task

---
+++ Task2_FriendsDog/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double count = 0;
+        double distance = 10000;
+        double firstFriendSpeed = 1;
+        double secondFriendSpeed = 2;
+        double dogSpeed = 5;
+        double friend = 2;
+        double time = 0;
+
+        while (distance > 10)
+        {
+            if (friend == 1)
+            {
+                time = distance / (firstFriendSpeed + dogSpeed);
+                friend = 2;
+                distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+                count = count + 1;
+
+            }
+            Console.WriteLine("second" + count);
+
+            if (friend == 2)
+
+            {
+                time = distance / (secondFriendSpeed + dogSpeed);
+                friend = 1;
+                distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+                count = count + 1;
+                //Console.WriteLine(count);
+            }
+            Console.WriteLine("First" + count);
+        }
+        Console.WriteLine(count);
+    }
+}
