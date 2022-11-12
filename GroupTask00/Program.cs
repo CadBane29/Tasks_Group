22e4e51f6f7132125b8574a3419f6df272fe2e1c
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-12 15:30:29 +0300

    add Group Task 00

---
+++ GroupTask00/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на
+// 1. вход принимает число и
+// 2. выдает его квадрат (число умноженное на само себя).
+// Например:
+// 4 -> 16
+// 3 -> 9
+// 7 -> 49
+
+Console.WriteLine("Введите целое число: ");
+string strNum = Console.ReadLine(); // "-7)
+int number = Convert.ToInt32(strNum);
+// int num = Convert.ToInt32(Console.ReadLine());
+
+int square = number * number;
+Console.WriteLine($"Квадрат числа  {number} = {square}");
