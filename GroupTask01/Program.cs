e1256700d0fbd1a4b5449d36ad4aad33206303c4
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-12 16:12:21 +0300

    add GroupTask1 Square Numbers

---
+++ GroupTask01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на 
+// 1. вход принимает два числа и 
+// 2. проверяет, является перове число квадратом второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да
+// a = -3, b = 9 -> нет
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2)
+{
+    Console.WriteLine($"Число {number1}, является квадратом числа {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {number1}, не ялвяется квадратом числа {number2}, введите другое число!");
+}
