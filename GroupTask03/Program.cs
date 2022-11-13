756e1d5b161cdb05a91cc91a569557d253a54fc9
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-13 23:50:43 +0300

    add GroupTask03 While N != -N

---
+++ GroupTask03/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая
+// 1. на вход принимает одно число (N), а 
+// 2. на выходе показывает все целые числа в промежутке от -N до N.
+Console.WriteLine("Введите целое число.");
+int N = Convert.ToInt32(Console.ReadLine());
+int Negative = N * -1;
+//Console.WriteLine(Negative);
+while (N != Negative)
+{
+    if (N > Negative)
+    {
+        N = N - 1;
+        Console.WriteLine(N);
+    }
+    else
+    {
+        N = N + 1;
+        Console.WriteLine(N);
+    }
+}
+Console.WriteLine("Промежуток чисел от натурального к противоположному натуральному.");
