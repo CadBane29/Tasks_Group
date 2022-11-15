58130a2f7e93758c448763e78f79869bd047869b
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-15 20:37:45 +0300

    add Group Task05 Two out Three

---
+++ GroupTask05_TwoOutOfThree/Program.cs
@@ -0,0 +1,21 @@
+﻿// 11. Напишите программу, которая 
+// 1. выводит случайное трёхзначное число и 
+// 2. удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Random number 100 - 999 --> {number}");
+
+int RemoveSecondDigit(int num)
+{
+    int firstDigit = num / 100;
+    int secondDigit = num % 10;
+    int result = firstDigit * 10 + secondDigit;
+    return result;
+}
+
+int removeSecondDigit = RemoveSecondDigit(number);
+Console.Write($"Output of number {removeSecondDigit}");
