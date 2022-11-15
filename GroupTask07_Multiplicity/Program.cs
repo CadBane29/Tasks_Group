131b45e6e7e849620ae17658507c4e2b5ea6ee59
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-15 23:39:16 +0300

    add GroupTask07_Multiplicity

---
+++ GroupTask07_Multiplicity/Program.cs
@@ -0,0 +1,17 @@
+﻿// 14. Напишите программу, которая 
+// 1. принимает на вход число и 
+// 2. проверяет, кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("User, please enter number.");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Multiplicity(int num1)
+{
+    return (num1 % 7 == 0) && (num1 % 23 == 0);
+}
+bool result = Multiplicity(number);
+Console.Write(result ? "Multiplicity" : "Not Multiplicity");
