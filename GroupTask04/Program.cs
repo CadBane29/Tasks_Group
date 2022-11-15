5a986f67a4b06a89e131fdeee784178c89e84ea8
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-15 18:42:21 +0300

    add Task04 Segment 10 - 99

---
+++ GroupTask04/Program.cs
@@ -0,0 +1,19 @@
+﻿// 9. Напишите программу, которая 
+// 1. выводит случайное число из отрезка [10, 99] и 
+// 2. показывает наибольшую цифру числа.
+
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Random number from segment 10 - 99 --> {number}");
+
+int firstDigit = number / 10; // 78 --> 7
+int secondDigit = number % 10; // 78 --> 8
+
+// При одной строчки условия, можно писать в одну строку
+if (firstDigit > secondDigit)  Console.WriteLine($"Larget digit of a number --> {firstDigit}");
+else Console.WriteLine($"Larget digit of a number --> {secondDigit}");
+
+//Console.WriteLine(Math.Max(firstDigit, secondDigit));
