f9d9b3767a30296f0b89c63db5658d7a8b0cb8a3
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-23 16:22:33 +0300

    git add GroupTask26 Number of values with number

---
+++ GroupTask26/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 26: Напишите программу, которая 
+// 1. принимает на вход число и 
+// 2. выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// int number = -1;
+
+// while (number < 0)
+// {
+//     Console.WriteLine("User, enter number: ");
+//     number = Convert.ToInt32(Console.ReadLine());
+//     if (number < 0) Console.WriteLine("Enter positive number!");
+// }
+
+// int Digits(int num)
+// {
+//     int count = 0;
+//     while (num > 0)
+//     {
+//         num = num / 10;
+//         count++;
+//     }
+//     return count;
+// }
+
+// int result = Digits(number);
+// Console.WriteLine($"Количество цифр в числе {number} ---> {result}");
+
+Console.WriteLine("User, enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Digits(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count == 0 ? 1 : count;
+}
+
+if (number < 0) number = -number; // Math.Abs(-123); записывает число в виде положительного
+int result = Digits(number);
+Console.WriteLine($"Количество цифр в числе {number} ---> {result}");
