d301258cfb9335c69acb08439282d429b13bdc99
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-11-29 14:04:09 +0300

    add GroupTask35 Find element from 10 to 99 in array

---
+++ GroupTask35/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] CreateArrayInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] array = CreateArrayInt(123, 1, 999);
+PrintArray(array);
+
+int FindElementArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 9 && array[i] < 100) count++;
+    }
+    return count;
+}
+
+int elementArray = FindElementArray(array);
+Console.WriteLine($"В данном массиве в отрезке от 10 до 99 --> {elementArray} значений.");
