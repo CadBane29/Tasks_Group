bbaf92cf51d06baf40f6e2cdfb026f9e3b653578
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-12-04 15:56:28 +0300

    add GroupTask46 Matrix array

---
+++ GroupTask46/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)  // чтобы пройтись по столбцам, а не по строкам нужно поменять местами
+    {                                              // i и j, а также GetLength(0 или 1)
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+            else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int rows = 3;
+int columns = 4;
+int[,] array2D = CreateMatrixRndInt(rows, columns, -99, 99);
+PrintMatrix(array2D);
