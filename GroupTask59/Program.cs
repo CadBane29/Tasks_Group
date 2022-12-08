3448b183222cd12d8cb9b975181e1aa7507bf272
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-12-08 17:03:24 +0300

    add GroupTask59 Delete row and column by matrix

---
+++ GroupTask59/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 59: Задайте двумерный массив из целых чисел.
+// Напишите программу, которая удалит строку и столбец, на
+// пересечении которых расположен наименьший элемент
+// массива.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("\n[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write("{0,4}", matrix[i, j]);
+            Console.Write(j < matrix.GetLength(1) - 1 ? ","
+                                            : string.Empty);
+        }
+        Console.Write(" ]\n");
+    }
+}
+
+void FindMinimalElement(int[,] matrix, out int indexRow, out int indexColumn) // out для передачи двух параметров
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    int min = matrix[0, 0];
+    indexColumn = 0;
+    indexRow = 0;
+    for (int i = 1; i < rows; i++)
+    {
+        for (int j = 1; j < columns; j++)
+        {
+            if (matrix[i, j] < min)
+            {
+                min = matrix[i, j];
+                indexRow = i;
+                indexColumn = j;
+            }
+        }
+    }
+}
+
+int[,] ChangesElementByMatrix(int[,] matrix, int indexRow, int indexColumn)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    int[,] matrix2 = new int[rows - 1, columns - 1];
+    for (int i = 0, m = 0; i < rows - 1; i++, m++)
+    {
+        if (m == indexRow) m++;
+        for (int j = 0, n = 0; j < columns - 1; j++, n++)
+        {
+            if (n == indexColumn) n++;
+            matrix2[i, j] = matrix[m, n];
+        }
+    }
+    return matrix2;
+
+}
+
+int[,] matrix = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(matrix);
+Console.WriteLine();
+FindMinimalElement(matrix, out int indexRow, out int indexColumn);
+int[,] minimal = ChangesElementByMatrix(matrix, indexRow, indexColumn);
+PrintMatrix(minimal);
