fbaddfecf8b4f224349f6c7bd6c3aed085ba2109
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-12-12 19:58:11 +0300

    add GroupTask65 Natural numbers from M to N solution recursion

---
+++ GroupTask65/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+
+Console.Write("Введите первое натуральное число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе натуральное число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+NaturalNumbers(firstNumber, secondNumber);
+
+
+void NaturalNumbers(int num1, int num2)
+{
+    if(num1 <= num2)
+    {
+        Console.Write($"{num1} ");
+        if(num1 == num2) return;
+        NaturalNumbers(num1 + 1, num2);              
+    }
+    else if(num1 >= num2)
+    {
+        Console.Write($"{num1} ");
+        if(num1 == num2) return;
+        NaturalNumbers(num1 - 1, num2);   
+    }
+
+}
