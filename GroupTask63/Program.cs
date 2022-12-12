27f4ea03e6d6d02ba19071f7ef30bffffb51b1b9
CadBane29 <115402030+CadBane29@users.noreply.github.com>
2022-12-12 17:47:54 +0300

    add in GroupTask63 Recursion begin, another solution

--- GroupTask63/Program.cs
+++ GroupTask63/Program.cs
@@ -8,9 +8,18 @@ Console.Write("Введите натуральное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 NaturalNumbers(number);
 
+// void NaturalNumbers(int num)
+// {
+//     if (num == 0) return;
+//     NaturalNumbers(num - 1);
+//     Console.Write($"{num} ");
+// }
+
 void NaturalNumbers(int num)
 {
-    if (num == 0) return;
-    NaturalNumbers(num - 1);
-    Console.Write($"{num} ");
+    if (num > 0)
+    {
+        NaturalNumbers(num - 1);
+        Console.Write($"{num} ");
+    }
 }
